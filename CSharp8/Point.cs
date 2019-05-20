18ade74ec55bb0d9e5b7f53c3fa8be8f24424e10
Oberdan Bitencourt Ferreira <oberdan.bitencourt@gmail.com>
2019-05-20 16:35:47 -0300

    C# 8 readonly members

--- CSharp8/Point.cs
+++ CSharp8/Point.cs
@@ -4,7 +4,9 @@ public struct Point
     public double X { get; set; }
     public double Y { get; set; }
     
-    public double Distance => Math.Sqrt(X * X + Y * Y);
+    // If you remove readonly from here you're going to get a compiler warning message:
+    // warning CS8656: Call to non-readonly member 'Point.Distance.get' from a 'readonly' member results in an implicit copy of 'this'
+    public readonly double Distance => Math.Sqrt(X * X + Y * Y);
 
-    public override string ToString() => $"({X}, {Y}) is {Distance} from the origin";
+    public readonly override string ToString() => $"({X}, {Y}) is {Distance} from the origin";
 }
