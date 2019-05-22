8867e847e45bb16680cd86e1b50027adb14f4cc5
Ricardo Dorta <ricardo.dorta@arctouch.com>
2019-05-22 20:08:32 -0300

    Added local static function sample

--- CSharp8/Program.cs
+++ CSharp8/Program.cs
@@ -6,7 +6,20 @@ namespace CSharp8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(DoSum());    
         }
+
+        static int DoSum()
+        {   
+            var x = 1;
+            var y = 2;
+
+            return Add(x,y);
+
+
+            static int Add(int first, int second) => first + second;
+        }
+
+        
     }
 }
