3db449ff750cd39fa6f097648ec9946512a052f8
Oberdan Bitencourt Ferreira <oberdan.bitencourt@gmail.com>
2019-05-22 16:53:37 -0300

    Refactored solution to hold all the new patterns all together.

--- CSharp8/Program.cs
+++ CSharp8/Program.cs
@@ -6,32 +6,96 @@ namespace CSharp8
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(@"Choose one of the following:
+            switch
+            property
+            tuple");
+            var option = Console.ReadLine();
+            var outcome = RunChosenOption(option);
+
+            string RunChosenOption(string option) =>
+            option switch
+            {
+                "switch" => RunSwitchExpressions(),
+                "property" => RunPropertyPatterns(),
+                "tuple" => RunTuplePatterns(),
+                _ => $"{option} is invalid."
+            };
+
+            Console.WriteLine(outcome);
+        }
+
+        static string RunSwitchExpressions()
+        {
             Console.WriteLine("Tell me your preferred language, i.e.: pt-br");
-            Console.WriteLine(GetGreeting(Console.ReadLine()));
+            string choice = Console.ReadLine();
+            return GetGreeting(choice);
         }
 
         static string GetGreeting(string language) =>
-            language switch
-            {
-                "pr-br" => "Oi!",
-                "en-us" => "Hello!",
-                "es-es" => "¡Hola!",
-                _ => "???????????"
-            };
-
-        static string GetGreetingOldWay(string language)
+        language switch
         {
-            switch (language)
-            {
-                case "pt-br":
-                    return "Oi!";
-                case "en-us":
-                    return "Hello!";
-                case "es-es":
-                    return "¡Hola!";
-                default:
-                    return "???????????";
-            }
+            "pt-br" => "Oi!",
+            "en-us" => "Hello!",
+            "es-es" => "¡Hola!",
+            _ => "👋"
+        };
+
+        static string RunPropertyPatterns()
+        {
+            Console.WriteLine("Tell me a currency you want to convert R$ to:");
+            var currency = Currency.FromName(Console.ReadLine());
+            Console.WriteLine("Tell me the amount");
+            decimal amount = Convert.ToDecimal(Console.ReadLine());
+            return ConvertBrlTo(currency, amount);
         }
+
+        public static string ConvertBrlTo(Currency currency, decimal amount) =>
+        currency switch
+        {
+            { Name: "Dollar", Sign: var sign } => $"The R${amount} in dollars is {sign}{(0.247856m * amount)}",
+            { Name: "Euro", Sign: var sign } => $"The R${amount} in euros is {sign}{(0.22183m * amount)}",
+            { Name: "Pound", Sign: var sign } => $"The R${amount} in pound sterling is {sign}{(0.19533m * amount)}",
+            _ => amount.ToString()
+        };
+
+        static string RunTuplePatterns()
+        {
+            Console.WriteLine("Let's play rock, paper, scissors");
+            Console.WriteLine("Select one of 👊, ✋, ✌️");
+            var firstChoice = Console.ReadLine();
+            Console.WriteLine("Select one of 👊, ✋, ✌️");
+            var secondChoice = Console.ReadLine();
+            return RockPaperScissors(firstChoice, secondChoice);
+        }
+
+        public static string RockPaperScissors(string first, string second)
+    => (first, second) switch
+    {
+        ("👊", "✋") => "rock is covered by paper. Paper wins.",
+        ("👊", "✌️") => "rock breaks scissors. Rock wins.",
+        ("✋", "👊") => "paper covers rock. Paper wins.",
+        ("✋", "✌️") => "️paper is cut by scissors. Scissors wins.",
+        ("✌️", "👊") => "scissors is broken by rock. Rock wins.",
+        ("✌️", "✋") => "scissors cuts paper. Scissors wins.",
+        (_, _) => "tie"
+    };
+    }
+
+    class Currency
+    {
+        public Currency(string name, string sign) => (Name, Sign) = (name, sign);
+        
+        public string Name { get; }
+        public string Sign { get; }
+
+        public static Currency FromName(string name) =>
+        name switch
+        {
+            "pound" => new Currency("Pound", "£"),
+            "dollar" => new Currency("Dollar", "$"),
+            "euro" => new Currency("Euro", "€"),
+            _ => new Currency("?", "?"),
+        };
     }
 }
